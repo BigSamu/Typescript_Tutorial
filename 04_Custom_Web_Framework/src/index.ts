1a14ba6e14e937c5e8cb16a754a73de005749e1c
bigsamu <valdesgutierrez@gmail.com>
2024-05-10 17:10:39 -0400

    updating task 04_custom_web_framework
    Signed-off-by: bigsamu <valdesgutierrez@gmail.com>

--- 04_Custom_Web_Framework/src/index.ts
+++ 04_Custom_Web_Framework/src/index.ts
@@ -1,6 +1,23 @@
 import { User } from "./models/User";
+import axios from "axios";
 
-const user = new User({name: 'myname', age: 20});
+const user = new User({ name: "myname", age: 20 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on("change", () => {
+  console.log("Change #1");
+});
+user.on("change", () => {
+  console.log("Change #2");
+});
+user.on("save", () => {
+  console.log("Save was triggerd");
+});
+
+// user.trigger('change');
+// user.trigger('save');
+user.trigger("sa");
+
+axios.post('http://localhost:3000/users', {
+  name: 'myname',
+  age: 20
+})
--- 04_Custom_Web_Framework/src/models/User.ts
+++ 04_Custom_Web_Framework/src/models/User.ts
@@ -1,12 +1,49 @@
+import axios, {AxiosPromise} from "axios";
 interface UserProps {
-  name: string;
-  age: number;
+  id?: number;
+  name?: string;
+  age?: number;
 }
 
-export class User {
-  constructor(private data: UserProps){};
+type Callback = () => void; // Typescript type alias
+// Defining Callback as a void function with no arguments
 
-  get(propName: string): (number | string) {
-    return this.data[propName]
+export class User {
+  events: { [key: string]: Callback[] } = {};
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if(!handlers || handlers.length === 0){
+      return;
+    }
+    handlers.forEach(callback => {
+      callback();
+    })
+  }
+
+  save(): void {
+
+  }
+
+  fetch(): void {
+    axios.get(`http://localhost:3000/users/${this.get('id')}`)
+    .then((response: AxiosResponse): void => {
+
+    });
   }
 }
