c5c6680d2319c7d186201fb3986edfd0a8d8f75c
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-05-27 16:41:20 -0400

    update 'Custom_Web_Framework'
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- 04_Custom_Web_Framework/src/index.ts
+++ 04_Custom_Web_Framework/src/index.ts
@@ -1,23 +1,5 @@
 import { User } from "./models/User";
-import axios from "axios";
 
-const user = new User({ name: "myname", age: 20 });
+const user = new User({ name: 'new record', age: 0 });
 
-user.on("change", () => {
-  console.log("Change #1");
-});
-user.on("change", () => {
-  console.log("Change #2");
-});
-user.on("save", () => {
-  console.log("Save was triggerd");
-});
-
-// user.trigger('change');
-// user.trigger('save');
-user.trigger("sa");
-
-axios.post('http://localhost:3000/users', {
-  name: 'myname',
-  age: 20
-})
+user.save();
