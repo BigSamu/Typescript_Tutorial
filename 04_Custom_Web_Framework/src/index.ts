fe0d563dba6b7be6a61ab55ff0992bbd4d397a4b
BigSamu <valdesgutierrez@gmail.com>
2024-06-23 19:48:04 -0400

    update section 04_Custom_Web_Frameworks
    Signed-off-by: BigSamu <valdesgutierrez@gmail.com>

--- 04_Custom_Web_Framework/src/index.ts
+++ 04_Custom_Web_Framework/src/index.ts
@@ -1,9 +1,40 @@
-import { User } from "./models/User";
+// --------------------------------------------------------
+// I) Single Form
+// --------------------------------------------------------
 
-const user = new User({ name: 'new record', age: 0 });
+// import { UserEdit } from "./views/UserEdit";
+// import { User } from "./models/User";
 
-user.events.on('change', () => {
-  console.log('change!');
-})
+// const user = User.buildUser({ name: "NAME", age: 20 });
+// const root = document.getElementById("root");
 
-user.events.trigger('change');
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+//   userEdit.render();
+//   console.log(userEdit);
+// } else {
+//   throw new Error('Root element not Found')
+// }
+
+// --------------------------------------------------------
+// II) Collection View
+// --------------------------------------------------------
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
+
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on("change", () => {
+  const root = document.getElementById("root");
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
--- 04_Custom_Web_Framework/src/models/User.ts
+++ 04_Custom_Web_Framework/src/models/User.ts
@@ -1,23 +1,33 @@
-import axios, { AxiosResponse } from "axios";
+import { Model } from "./Model";
+import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
+import { ApiSync } from "./ApiSync";
+import { Collection } from "./Collection";
+
 export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
-export class User {
-  events: Eventing = new Eventing();
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
+const rootUrl = "http://localhost:3000/users";
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
   }
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
+    );
   }
 
-
-
-
+  setRandomAge(): void {
+    const age = Math.round(Math.random() * 100);
+    this.set({ age });
+  }
 }
